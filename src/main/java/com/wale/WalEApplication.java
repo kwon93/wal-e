c7da8ceef00ca6b893ef6fd3435babb32d42220b
권동혁 <kdh@2isys.com>
2025-10-20 17:58:21 +0900

    wal-e

---
+++ src/main/java/com/wale/WalEApplication.java
@@ -0,0 +1,13 @@
+package com.wale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalEApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wale/WalEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
